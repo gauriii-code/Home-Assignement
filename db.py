332566ac86ee4bc724f6af2b2a105a2e8cd5b03f
gauriii-code <gauriii769@gmail.com>
2025-09-16 11:59:15 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/lumibot_db")
+
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(bind=engine)
